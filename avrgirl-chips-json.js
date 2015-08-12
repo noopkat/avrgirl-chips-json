82feec6f7f636f609e1612eb9c89e979ef82e79a
Suz Hinton <suz.hinton@gmail.com>
2015-08-11 22:41:38 -0400

    add package goods

---
+++ avrgirl-chips-json.js
@@ -0,0 +1,7 @@
+var chips = {};
+
+chips.attiny44 = require('attiny/attiny44');
+chips.attiny45 = require('attiny/attiny45');
+chips.attiny85 = require('attiny/attiny85');
+
+module.exports = chips;
