90c03c41c72350580fae4a3427699f25d9161400
Suz Hinton <suz.hinton@gmail.com>
2015-08-11 22:55:53 -0400

    fix some ref nits

--- avrgirl-chips-json.js
+++ avrgirl-chips-json.js
@@ -1,7 +1,7 @@
 var chips = {};
 
-chips.attiny44 = require('attiny/attiny44');
-chips.attiny45 = require('attiny/attiny45');
-chips.attiny85 = require('attiny/attiny85');
+chips.attiny44 = require('./attiny/attiny44');
+chips.attiny45 = require('./attiny/attiny45');
+chips.attiny84 = require('./attiny/attiny84');
 
 module.exports = chips;
